bd804cd14d26dc49af76f5bce71b3a87bf1cc97a
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2022-11-25 16:21:21 -0300

    test: 6 fails, before debug

--- src/helpers/Field.ts
+++ src/helpers/Field.ts
@@ -1,3 +1,4 @@
+import { incrementNeighbours } from "./CellsManipulator";
 export type Cell = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 export type Field = Cell[][];
 export type Coords = [number, number];
@@ -18,7 +19,7 @@ export const emptyFieldGenerator = (
 };
 
 export const fieldGenerator = (size: number, probability: number): Field => {
-  if (probability > 1 || probability < 0)
+  if (probability < 0 || probability > 1)
     throw new Error("Probability must be between 0 and 1");
 
   let unprocessedCells = size * size;
@@ -26,11 +27,13 @@ export const fieldGenerator = (size: number, probability: number): Field => {
 
   const result: Field = emptyFieldGenerator(size);
 
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      if (restCellsWithBombs === 0) result;
+  for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+      // if (restCellsWithBombs === 0) return result;
+
       if (restCellsWithBombs / unprocessedCells > Math.random()) {
-        result[i][j] = CellState.bomb;
+        result[y][x] = CellState.bomb;
+        incrementNeighbours([y, x], result);
         restCellsWithBombs--;
       }
       unprocessedCells--;
