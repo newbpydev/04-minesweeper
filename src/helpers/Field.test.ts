9e25ee053f7f2affbf4e2fbbba6a9745dccef690
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2022-11-25 14:09:41 -0300

    test: tests for 50% prob, linear logic

--- src/helpers/Field.test.ts
+++ src/helpers/Field.test.ts
@@ -1,4 +1,4 @@
-import { emptyFieldGenerator, CellState } from "./Field";
+import { emptyFieldGenerator, fieldGenerator, CellState } from "./Field";
 
 const { empty, bomb, hidden } = CellState;
 
@@ -22,6 +22,81 @@ describe("Field Generator", () => {
         [hidden, hidden, hidden],
         [hidden, hidden, hidden],
         [hidden, hidden, hidden],
+      ]);
+    });
+  });
+
+  describe("Simple cases", () => {
+    // @ wrong probability test
+    it("Wrong probability", () => {
+      const errorText = "Probability must be between 0 and 1";
+      expect(() => fieldGenerator(1, -1)).toThrow(errorText);
+      expect(() => fieldGenerator(1, 2)).toThrow(errorText);
+    });
+    // @ smallest possible field without mine
+    it("Smallest possible field without mines", () => {
+      expect(fieldGenerator(1, 0)).toStrictEqual([[empty]]);
+    });
+    // @ big possible field without mine
+    it("Big field without mines", () => {
+      expect(fieldGenerator(10, 0)).toStrictEqual([
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+      ]);
+    });
+    // @ smallest possible field with mine
+    it("Smallest possible field with mines", () => {
+      expect(fieldGenerator(1, 1)).toStrictEqual([[bomb]]);
+    });
+    // @ 2x2 field with mine
+    it("2x2 field with mines", () => {
+      expect(fieldGenerator(2, 1)).toStrictEqual([
+        [bomb, bomb],
+        [bomb, bomb],
+      ]);
+    });
+    // @ 2x2 field with 50% probability
+    it("2x2 field with 50% probability", () => {
+      const field = fieldGenerator(2, 0.5);
+      const flatField = field.flat();
+
+      console.table(field); //?
+      console.table(flatField); //?
+
+      const cellsWithBombs = flatField.filter((cell) => cell === bomb); //?
+      const emptyCells = flatField.filter((cell) => cell === empty); //?
+
+      expect(cellsWithBombs).toHaveLength(2);
+      expect(emptyCells).toHaveLength(2);
+
+      // expect(fieldGenerator(2, 1)).toStrictEqual([
+      //   [bomb, bomb],
+      //   [bomb, bomb],
+      // ]);
+    });
+
+    // @ 2x2 field with 50% probability
+    it("2x2 field with 50% probability", () => {
+      expect(fieldGenerator(2, 0.5)).toStrictEqual([
+        [bomb, bomb],
+        [empty, empty],
+      ]);
+    });
+    // @ 4x4 field with 50% probability
+    it("4x4 field with 50% probability", () => {
+      expect(fieldGenerator(4, 0.5)).toStrictEqual([
+        [bomb, bomb, bomb, bomb],
+        [bomb, bomb, bomb, bomb],
+        [empty, empty, empty, empty],
+        [empty, empty, empty, empty],
       ]);
     });
   });
